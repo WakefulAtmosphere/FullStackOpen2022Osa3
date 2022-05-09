36e10594757e92109a68041c41f555f0df5c3235
WakefulAtmosphere <60845785+WakefulAtmosphere@users.noreply.github.com>
2022-05-09 14:26:24 +0300

    frontend maybe added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 var morgan = require('morgan')
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('build'))
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
 
@@ -35,7 +35,7 @@ let persons = [
 ]
 
 app.get('/', morgan('tiny'), (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send()
 })
 
 app.get('/api/persons', morgan('tiny'), (req, res) => {
