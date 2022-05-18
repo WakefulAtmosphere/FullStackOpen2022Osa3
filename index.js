20378463049059818fec3e78328fab1974bc46ec
WakefulAtmosphere <60845785+WakefulAtmosphere@users.noreply.github.com>
2022-05-18 21:00:38 +0300

    mongodb integration

--- index.js
+++ index.js
@@ -1,90 +1,112 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const Person = require('./models/person')
 
 var morgan = require('morgan')
-app.use(express.json())
-app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
+
+app.use(cors())
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
+const errorHandler = (error, req, res, next) => {
+  //console.error(error.name)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error })
+  }
 
 
+  next(error)
+}
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-]
 
 app.get('/', morgan('tiny'), (req, res) => {
   res.send()
 })
 
 app.get('/api/persons', morgan('tiny'), (req, res) => {
-  res.json(persons)
+  Person.find({}).then(result => {
+    res.json(result)
+  })
 })
 
 app.get('/info', morgan('tiny'), (req, res) => {
   const date = Date()
+  Person.find({}).then(result => {
   res.send(`<div>
-              <p>phonebook has info for ${persons.length} people</p>
+              <p>phonebook has info for ${result.length} people</p>
               <p>${date}</p>
             </div>`)
-})
-
-app.get('/api/persons/:id', morgan('tiny'), (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
   }
+)})
+
+app.get('/api/persons/:id', morgan('tiny'), (req, res, next) => {
+    Person.findById(req.params.id).then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
-app.delete('/api/persons/:id', morgan('tiny'), (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(note => note.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', morgan('tiny'), (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      if (result) {
+        res.status(204).end()
+      } else {
+        throw "CastError"
+      }
+    })
+    .catch(error => next(error))
 })
 
 
-app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :data'), (req, res) => {
-  let person = req.body
-  if (!(person.name && person.number)) {
-    return res.status(400).json({ 
-      error: 'content missing' 
+app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :data'), (req, res, next) => {
+  let body = req.body
+  const person = new Person({
+    "name": body.name,
+    "number": body.number
+  })
+  person.save()
+    .then(savedPerson => {
+    res.json(savedPerson)
     })
-  } else if (persons.some(n => n.name === person.name)) {
-    return res.status(400).json({ 
-      error: 'entry already exists' 
-    })
+    .catch(error => {
+      next(error)})
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
   }
-  person = {...person, "id": Math.floor(Math.random()*10000)}
-  persons = persons.concat(person)
-  res.send(person)
-  res.status(200).end()
+  Person.findByIdAndUpdate(req.params.id, person, { runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      console.log(updatedPerson)
+      if (updatedPerson) {
+        res.json(updatedPerson)
+      } else {
+        throw "CastError"
+      }
+    })
+    .catch(
+      error => {
+        next(error)
+        }
+    )
 })
-
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
