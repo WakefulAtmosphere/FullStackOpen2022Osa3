b5568a1cc5327827e8fe020479c9b16ffec95746
WakefulAtmosphere <60845785+WakefulAtmosphere@users.noreply.github.com>
2022-05-19 14:41:36 +0300

    add linter and conform code to it

--- index.js
+++ index.js
@@ -1,113 +1,112 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Person = require('./models/person')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const Person = require('./models/person');
 
-var morgan = require('morgan')
-app.use(express.static('build'))
-app.use(express.json())
+var morgan = require('morgan');
+app.use(express.static('build'));
+app.use(express.json());
 
-app.use(cors())
-morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
+app.use(cors());
+morgan.token('data', function (req) { return JSON.stringify(req.body); });
 const errorHandler = (error, req, res, next) => {
   //console.error(error.name)
 
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
+    return res.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error })
+    return res.status(400).json({ error: error });
   }
 
 
-  next(error)
-}
+  next(error);
+};
 
 
 app.get('/', morgan('tiny'), (req, res) => {
-  res.send()
-})
+  res.send();
+});
 
 app.get('/api/persons', morgan('tiny'), (req, res) => {
   Person.find({}).then(result => {
-    res.json(result)
-  })
-})
+    res.json(result);
+  });
+});
 
 app.get('/info', morgan('tiny'), (req, res) => {
-  const date = Date()
+  const date = Date();
   Person.find({}).then(result => {
-  res.send(`<div>
+    res.send(`<div>
               <p>phonebook has info for ${result.length} people</p>
               <p>${date}</p>
-            </div>`)
-  }
-)})
+            </div>`);
+  });
+});
 
 app.get('/api/persons/:id', morgan('tiny'), (req, res, next) => {
-    Person.findById(req.params.id).then(person => {
-      if (person) {
-        res.json(person)
-      } else {
-        res.status(404).end()
-      }
-    })
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  })
     .catch(error => {
-      next(error)
-    })
-})
+      next(error);
+    });
+});
 
 app.delete('/api/persons/:id', morgan('tiny'), (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(result => {
       if (result) {
-        res.status(204).end()
+        res.status(204).end();
       } else {
-        throw "CastError"
+        throw 'CastError';
       }
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 
 app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :data'), (req, res, next) => {
-  let body = req.body
+  let body = req.body;
   const person = new Person({
-    "name": body.name,
-    "number": body.number
-  })
+    'name': body.name,
+    'number': body.number
+  });
   person.save()
     .then(savedPerson => {
-    res.json(savedPerson)
+      res.json(savedPerson);
     })
     .catch(error => {
-      next(error)})
-})
+      next(error);});
+});
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+  const body = req.body;
   const person = {
     name: body.name,
     number: body.number,
-  }
+  };
   Person.findByIdAndUpdate(req.params.id, person, { runValidators: true, context: 'query' })
     .then(updatedPerson => {
-      console.log(updatedPerson)
+      console.log(updatedPerson);
       if (updatedPerson) {
-        res.json(updatedPerson)
+        res.json(updatedPerson);
       } else {
-        throw "CastError"
+        throw 'CastError';
       }
     })
     .catch(
       error => {
-        next(error)
-        }
-    )
-})
-app.use(errorHandler)
-const PORT = process.env.PORT || 3001
+        next(error);
+      }
+    );
+});
+app.use(errorHandler);
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- models/person.js
+++ models/person.js
@@ -1,49 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then(() => {
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: [3, 'Name must be at least 3 characters long'],
-      required: [true, 'Both fields must be filled'],
-    },
-    number: {
-      type: String,
-      required: [true, 'Both fields must be filled'],
-      minLength: [8, 'Number must be at least 8 characters long'],
-      
-    },
-})
+  name: {
+    type: String,
+    minLength: [3, 'Name must be at least 3 characters long'],
+    required: [true, 'Both fields must be filled'],
+  },
+  number: {
+    type: String,
+    required: [true, 'Both fields must be filled'],
+    minLength: [8, 'Number must be at least 8 characters long'],
+  },
+});
 let validator = v => {
-  return /^\d{2,3}-\d+$/.test(v)
-}
+  return /^\d{2,3}-\d+$/.test(v);
+};
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
-personSchema.path('number').validate(validator, "Invalid format for number")
+});
+personSchema.path('number').validate(validator, 'Invalid format for number');
 
-
-/*
-validation: {
-        validator: function(v) {
-          console.log(v)
-          return /\d{2,3}-\d+/.test(v)
-        },
-        message: props => `${props.value} is not a valid phone number!`
-      },*/
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
